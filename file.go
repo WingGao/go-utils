b01a723f4207f051401f43ef46bf1732d3cf3e9f
Wing Gao <wing.gao@live.com>
2018-02-19 21:18:05 +0800

    添加BinExist

--- file.go
+++ file.go
@@ -6,6 +6,7 @@ import (
 	"encoding/base64"
 	"path/filepath"
 	"os"
+	"os/exec"
 )
 
 func FileGetBase64(filenameOrURL string, timeout ...time.Duration) (out string, err error) {
@@ -21,4 +22,17 @@ func FileGetBase64(filenameOrURL string, timeout ...time.Duration) (out string, 
 func BinPath() string {
 	p, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	return p
+}
+
+// 可执行文件是否存在
+func BinExist(binfile string, canPainc bool) bool {
+	cmd := exec.Command("which", binfile)
+	out, err := cmd.Output()
+	if err != nil || len(out) == 0 {
+		if canPainc {
+			panic("no such bin file: " + binfile)
+		}
+		return false
+	}
+	return true
 }
