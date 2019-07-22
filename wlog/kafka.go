7c9c38131444691c61fca6fab7735231f70133de
Wing Gao <wing.gao@live.com>
2019-07-22 15:36:13 +0800

    feat: update log caller

--- wlog/kafka.go
+++ wlog/kafka.go
@@ -1,19 +1,19 @@
 package wlog
 
 import (
-	"fmt"
 	"github.com/Shopify/sarama"
 	ucore "github.com/WingGao/go-utils/core"
-	"github.com/go-errors/errors"
 	"github.com/ungerik/go-dry"
 	"go.uber.org/zap/zapcore"
-	"strings"
 	"time"
 )
 
 func NewZapToKafka(producer sarama.AsyncProducer, topic, appid, logName string) *ZapToKafka {
-	return &ZapToKafka{producer: producer, topic: topic, appid: appid, name: dry.RealNetIP(),
+	alogger := &ZapToKafka{producer: producer, topic: topic, appid: appid, name: dry.RealNetIP(),
 		logName: logName}
+
+	alogger.encoder = getFullEncoder()
+	return alogger
 }
 
 type ZapToKafka struct {
@@ -22,6 +22,7 @@ type ZapToKafka struct {
 	name     string
 	logName  string
 	producer sarama.AsyncProducer
+	encoder  zapcore.Encoder
 }
 
 func (c *ZapToKafka) Enabled(zapcore.Level) bool {
@@ -33,17 +34,24 @@ func (c *ZapToKafka) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore
 }
 
 func (c *ZapToKafka) Write(ent zapcore.Entry, fields []zapcore.Field) error {
-	e := errors.Wrap(error(nil), 3)
-	var linef errors.StackFrame
-	for _, l := range e.Callers() {
-		linef = errors.NewStackFrame(l)
-		//fmt.Println(linef)
-		if !strings.HasPrefix(linef.Package, "go.uber.org") && !strings.Contains(linef.File, "mod/go.uber.org") {
-			break
-		}
+	/*
+		{/Users/ppd-03020144/Projs/go-web/src/github.com/WingGao/go-utils/wlog/kafka.go 36 (*ZapToKafka).Write github.com/WingGao/go-utils/wlog 79087703}
+		{/Users/ppd-03020144/go/pkg/mod/go.uber.org/zap@v1.10.0/zapcore/entry.go 215 (*CheckedEntry).Write go.uber.org/zap/zapcore 77411577}
+		{/Users/ppd-03020144/go/pkg/mod/go.uber.org/zap@v1.10.0/sugar.go 234 (*SugaredLogger).log go.uber.org/zap 77519217}
+		{/Users/ppd-03020144/go/pkg/mod/go.uber.org/zap@v1.10.0/sugar.go 138 GetCacheCloud github.com/WingGao/go-utils/redis 79969412}
+		{/Users/ppd-03020144/go/pkg/mod/go.uber.org/zap@v1.10.0/sugar.go 138 (*SugaredLogger).Infof go.uber.org/zap 79969327}
+		{/Users/ppd-03020144/Projs/go-web/src/github.com/WingGao/go-utils/redis/redis.go 262 NewRedisClient github.com/WingGao/go-utils/redis 79970883}
+		{/Users/ppd-03020144/Projs/go-web/src/github.com/WingGao/go-utils/redis/redis.go 253 LoadClient github.com/WingGao/go-utils/redis 87070099}
+		{/Users/ppd-03020144/Projs/go-web/src/github.com/WingGao/go-utils/redis/redis.go 252 LoadClient github.com/WingGao/go-utils/redis 87061781}
+		{/Users/ppd-03020144/Projs/go-web/wingao.net/webproj/mcmd/main.go 25 main main 87090345}
+		{/usr/local/Cellar/go/1.12.5/libexec/src/runtime/proc.go 200 main runtime 67310428}
+		{/usr/local/Cellar/go/1.12.5/libexec/src/runtime/asm_amd64.s 1337 goexit runtime 67498673}
+	*/
+	bf, err := c.encoder.EncodeEntry(ent, fields)
+	if err != nil {
+		return err
 	}
-	//linef := errors.NewStackFrame(e.Callers()[0])
-	strMsg := fmt.Sprintf("%s:%d\n    %s", linef.File, linef.LineNumber, ent.Message)
+	strMsg := bf.String()
 	now := time.Now()
 	lmsg := &logMessage{
 		AppId:         c.appid,
--- wlog/zap_iris.go
+++ wlog/zap_iris.go
@@ -1,12 +1,10 @@
 package wlog
 
 import (
-	"fmt"
-	"github.com/go-errors/errors"
 	"github.com/kataras/golog"
 	"github.com/kataras/iris"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"strings"
 )
 
 var (
@@ -18,14 +16,19 @@ var (
 		zapcore.PanicLevel:  golog.FatalLevel,
 		zapcore.DPanicLevel: golog.FatalLevel,
 	}
+
+	fullEncoder = zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
 )
 
 func NewZapToIris(app *iris.Application) *ZapToIris {
-	return &ZapToIris{app: app}
+	alogger := &ZapToIris{app: app}
+	alogger.encoder = getFullEncoder()
+	return alogger
 }
 
 type ZapToIris struct {
-	app *iris.Application
+	app     *iris.Application
+	encoder zapcore.Encoder
 }
 
 func (c *ZapToIris) Enabled(zapcore.Level) bool {
@@ -37,25 +40,30 @@ func (c *ZapToIris) Check(ent zapcore.Entry, ce *zapcore.CheckedEntry) *zapcore.
 }
 
 func (c *ZapToIris) Write(ent zapcore.Entry, fields []zapcore.Field) error {
-	e := errors.Wrap(error(nil),3)
+	//e := errors.Wrap(error(nil), 3)
 	//e := errors.New(error(nil))
 	// 0 /Users/ppd-03020144/Projs/go-web/src/github.com/WingGao/go-utils/wlog/zap_iris.go
 	// 1 /Users/ppd-03020144/go/pkg/mod/go.uber.org/zap@v1.10.0/zapcore/entry.go
 	// 2 /Users/ppd-03020144/go/pkg/mod/go.uber.org/zap@v1.10.0/sugar.go
 	// 3 /Users/ppd-03020144/go/pkg/mod/go.uber.org/zap@v1.10.0/sugar.go
 	//e.StackFrames()
-	var linef errors.StackFrame
-	for _, l := range e.Callers() {
-		linef = errors.NewStackFrame(l)
-		//fmt.Println(linef)
-		if !strings.HasPrefix(linef.Package, "go.uber.org") && !strings.Contains(linef.File, "mod/go.uber.org") {
-			break
-		}
+	//var linef errors.StackFrame
+	//for _, l := range e.Callers() {
+	//	linef = errors.NewStackFrame(l)
+	//	//fmt.Println(linef)
+	//	if !strings.HasPrefix(linef.Package, "go.uber.org") && !strings.Contains(linef.File, "mod/go.uber.org") {
+	//		break
+	//	}
+	//}
+	bf, err := c.encoder.EncodeEntry(ent, fields)
+	if err != nil {
+		return err
 	}
+	strMsg := bf.String()
 	//linef := errors.NewStackFrame(e.Callers()[0])
 	// 转换level
 	il := zapToIrisLevelMap[ent.Level]
-	c.app.Logger().Log(il, fmt.Sprintf("%s:%d\n    ", linef.File, linef.LineNumber), ent.Message)
+	c.app.Logger().Log(il, strMsg, ent.Message)
 	return nil
 }
 
@@ -65,4 +73,23 @@ func (c *ZapToIris) Sync() error {
 
 func (c *ZapToIris) With([]zapcore.Field) zapcore.Core {
 	return c
+}
+
+func getFullEncoder() zapcore.Encoder {
+	cnf := zapcore.EncoderConfig{
+		// Keys can be anything except the empty string.
+		//TimeKey:        "T",
+		//LevelKey:       "L",
+		NameKey:        "N",
+		CallerKey:      "C",
+		MessageKey:     "M",
+		StacktraceKey:  "S",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder,
+	}
+	encoder := zapcore.NewConsoleEncoder(cnf)
+	return encoder
 }
