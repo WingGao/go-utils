6e919458620a1effcd3065cb2282cf1954000ade
WingGao <wing.gao@live.com>
2019-10-10 13:13:03 +0800

    feat: where

--- file.go
+++ file.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"time"
 	"github.com/ungerik/go-dry"
 	"encoding/base64"
@@ -26,10 +27,11 @@ func BinPath() string {
 
 // 可执行文件是否存在
 func BinExist(binfile string, canPainc bool) bool {
-	cmd := exec.Command("which", binfile)
+	cmd := exec.Command("where", binfile)
 	out, err := cmd.Output()
 	if err != nil || len(out) == 0 {
 		if canPainc {
+			fmt.Println(err, out)
 			panic("no such bin file: " + binfile)
 		}
 		return false
