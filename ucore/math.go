d3cd296f6478aa822af9e5ff043d56b41b1c093f
WingGao <wing.gao@live.com>
2020-04-20 17:48:31 +0800

    feat: add maxint

--- ucore/math.go
+++ ucore/math.go
@@ -23,6 +23,9 @@ func MinUint64(a, b uint64) uint64 {
 func MaxUint32(a, b uint32) uint32 {
 	return uint32(math.Max(float64(a), float64(b)))
 }
+func MaxInt(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
 func MaxInt64(a, b int64) int64 {
 	return int64(math.Max(float64(a), float64(b)))
 }
