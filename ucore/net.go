1a113de9895baf464d29c0899f0041c45ff4e633
WingGao <wing.gao@live.com>
2020-09-21 22:54:25 +0800

    feat: update

--- ucore/net.go
+++ ucore/net.go
@@ -1,8 +1,12 @@
 package ucore
 
 import (
+	"fmt"
 	"github.com/parnurzeal/gorequest"
+	"golang.org/x/net/proxy"
 	"net"
+	"net/http"
+	"net/url"
 	"regexp"
 )
 
@@ -14,4 +18,32 @@ func GetRealIP() net.IP {
 	r, _ := regexp.Compile(`\[([\d+\.]+)\]`)
 	ips := r.FindStringSubmatch(body)
 	return net.ParseIP(ips[1])
+}
+
+type Proxy struct {
+	Type string
+	Host string
+	Port int
+}
+
+func (p Proxy) ApplyGorequest(req *gorequest.SuperAgent) error {
+	if p.Host != "" {
+		pu := fmt.Sprintf("%s://%s:%d", p.Type, p.Host, p.Port)
+		switch p.Type {
+		case "socks5":
+			u, e := url.Parse(pu)
+			if e != nil {
+				return e
+			}
+			socks5Dialer, e1 := proxy.FromURL(u, proxy.Direct)
+			if e1 != nil {
+				return e
+			}
+			req.Transport = &http.Transport{Dial: socks5Dialer.Dial}
+		case "http", "https":
+			req.Proxy(pu)
+		}
+	}
+
+	return nil
 }
